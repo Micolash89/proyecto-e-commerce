7c0bbeb2db909d42a74c7267ce70b40a0c3cb888
Leonel Quiroga <leonelequiroga1@gmail.com>
2023-10-30 17:30:59 -0300

    4commit

--- src/main/java/com/caballerosGuardiaReal/ecommerce/SeguridadWeb.java
+++ src/main/java/com/caballerosGuardiaReal/ecommerce/SeguridadWeb.java
@@ -1,4 +1,4 @@
-//package com.caballerosGuardiaReal.ecommerce;
+  //package com.caballerosGuardiaReal.ecommerce;
 //
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.context.annotation.Bean;
